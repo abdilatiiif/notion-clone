264107de0f3eae698e101e3f773ce8ecc9f19ca1
Latif <latif_1995@live.no>
2025-10-13 08:43:52 +0200

    sidebar start

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,36 @@
+"use client";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
+
 function Header() {
-  return <div>Header</div>;
+  const { user } = useUser();
+
+  console.log("USER", user);
+
+  return (
+    <div className="flex items-center justify-between p-5">
+      {user && (
+        <h1 className="text-2xl font-bold">
+          {user?.firstName} {`'s`} space
+        </h1>
+      )}
+
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </div>
+  );
 }
 export default Header;
---
+++ components/SideBar.tsx
@@ -0,0 +1,45 @@
+import { MenuIcon } from "lucide-react";
+import NewDocumentButton from "./NewDocumentButton";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+function SideBar() {
+  const menuOptions = (
+    <>
+      <NewDocumentButton />
+    </>
+  );
+
+  return (
+    <div className="p-2 md:p-5 bg-gray-200 relative">
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon />
+          </SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              {menuOptions}
+              <SheetDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </SheetDescription>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+      <div className="hidden md:inline">
+        <NewDocumentButton />
+      </div>
+    </div>
+  );
+}
+export default SideBar;
